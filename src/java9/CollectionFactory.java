9df14cc4834d5ddd059b2b4d4d02af7a92770510
sicky <chandra.23.jai@gmail.com>
2025-07-14 07:53:20 +0530

    add commentss

--- src/java9/CollectionFactory.java
+++ src/java9/CollectionFactory.java
@@ -105,8 +105,9 @@ public class CollectionFactory {
         List<String> one = List.of("Epl");
         List<String> two = List.of("Epl","LaLiga");
         List<String> ten = List.of("a","b","c","d","e","f","g","h","i","j");
+        
         //emptyList.add("kkk");
-        System.out.println(emptyList); // through exception java.base/java.util.ImmutableCollections.uoe
+        //System.out.println(emptyList); // through exception java.base/java.util.ImmutableCollections.uoe
         //UnsupportedOperationException
         //leagues.add("Ligue 1");
         //leagues.set(0,"SerieA");
